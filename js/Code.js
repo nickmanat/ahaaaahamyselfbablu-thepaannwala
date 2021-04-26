8cd34ac4f9fc04399a80e5a22b60311a63ee93f7
nickmanat <83219688+nickmanat@users.noreply.github.com>
2021-04-26 17:08:00 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("NFCUITNO", 100,82.8424811333)
+fill("lightblue")
+text("Hint : iplements and anthropoligically stable task" , 100,100.9819915080)  
+
+}
